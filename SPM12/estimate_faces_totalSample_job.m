b9b1a1631853611c356a635665eb850a86f60f0b
Gabriela L Suarez <127572000+gabrielalsuarez@users.noreply.github.com>
2023-06-14 14:00:24 -0400

    Update estimate_faces_totalSample_job.m

--- SPM12/estimate_faces_totalSample_job.m
+++ SPM12/estimate_faces_totalSample_job.m
@@ -3,12 +3,6 @@
 % spm SPM - SPM12 (6906)
 % cfg_CAasicio BasicIO - Unknown
 %-----------------------------------------------------------------------
-matlabbatch{1}.spm.stats.fmri_est.spmmat = {'/nfs/turbo/lsa-lukehyde/projects/gabby_faces_request_05122021/spm_analyses/main_effects_scan-cov/con_0003/SPM.mat'};
-matlabbatch{1}.spm.stats.fmri_est.write_residuals = 0;
-matlabbatch{1}.spm.stats.fmri_est.method.Classical = 1;
-matlabbatch{2}.spm.stats.fmri_est.spmmat = {'/nfs/turbo/lsa-lukehyde/projects/gabby_faces_request_05122021/spm_analyses/main_effects_scan-cov/con_0006/SPM.mat'};
-matlabbatch{2}.spm.stats.fmri_est.write_residuals = 0;
-matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
 matlabbatch{3}.spm.stats.fmri_est.spmmat = {'/nfs/turbo/lsa-lukehyde/projects/gabby_faces_request_05122021/spm_analyses/main_effects_scan-cov/con_0009/SPM.mat'};
 matlabbatch{3}.spm.stats.fmri_est.write_residuals = 0;
 matlabbatch{3}.spm.stats.fmri_est.method.Classical = 1;
